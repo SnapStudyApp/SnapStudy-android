70d7a2d147f92918eadddf0413d4d169ec95f054
Ekanshh Praveen <edpravee@uwaterloo.ca>
2022-06-07 01:08:21 -0400

    Built CardGestureListener for swipe detection

--- app/src/main/java/com/example/snapstudy_android/fragments/StudyFragment.kt
+++ app/src/main/java/com/example/snapstudy_android/fragments/StudyFragment.kt
@@ -1,33 +1,38 @@
 package com.example.snapstudy_android.fragments
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.os.Bundle
+import android.view.*
+import androidx.core.view.GestureDetectorCompat
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.snapstudy_android.R
+import kotlinx.android.synthetic.main.fragment_study.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [StudyFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StudyFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var frontAnim: AnimatorSet
+    lateinit var backAnim: AnimatorSet
+    private lateinit var detector: GestureDetectorCompat
+    var isFront = true
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        //setting scale
+        var scale:Float= resources.displayMetrics.density
+        cvAnswer.cameraDistance = 8000*scale
+        cvPrompt.cameraDistance=8000*scale
+
+        //front animation
+        frontAnim=AnimatorInflater.loadAnimator(activity,R.animator.front_animator) as AnimatorSet
+        backAnim=AnimatorInflater.loadAnimator(activity,R.animator.back_animator) as AnimatorSet
+
+        //setting the swipe detector
+        detector = GestureDetectorCompat(activity,CardGestureListener())
+
     }
 
     override fun onCreateView(
@@ -35,26 +40,54 @@ class StudyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_study, container, false)
+
+        val studyView = inflater.inflate(R.layout.fragment_study,container,false)
+
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StudyFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StudyFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+//    override fun onTouchEvent(event:MotionEvent?):Boolean{
+//        if (detector.onTouchEvent())
+//    }
+
+    inner class CardGestureListener: GestureDetector.SimpleOnGestureListener(){
+        private val SWIPE_THRESHOLD = 100
+        private val SWIPE_VELOCITY_THRESHOLD = 100
+        override fun onFling(
+            downEvent: MotionEvent?,
+            moveEvent: MotionEvent?,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            var diffX=moveEvent?.x?.minus(downEvent!!.x) ?:0.0F
+            var diffY=moveEvent?.y?.minus(downEvent!!.y) ?:0.0F
+
+            if(Math.abs(diffX)>Math.abs(diffY)){
+                //this is a left or right swipe
+                if(Math.abs(diffX)>SWIPE_THRESHOLD && Math.abs(velocityX)>SWIPE_VELOCITY_THRESHOLD){
+                    if(diffX>0){
+                        //right swipe
+                        this@StudyFragment.onRightSwipe()
+                    }
+
+                    else{
+                        //left swipe
+                        this@StudyFragment.onLeftSwipe()
+                    }
                 }
             }
+
+            return super.onFling(downEvent, moveEvent, velocityX, velocityY)
+        }
     }
+
+    private fun onLeftSwipe() {
+        TODO("Not yet implemented")
+    }
+
+    private fun onRightSwipe() {
+        TODO("Not yet implemented")
+    }
+
 }
