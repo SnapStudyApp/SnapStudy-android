bb3f51f688587dccd3f1d90a71208fa4d6e4952b
Ekanshh Praveen <edpravee@uwaterloo.ca>
2022-06-16 16:22:09 -0400

    Added hiding text functionality for flashcards

--- app/src/main/java/com/example/snapstudy_android/fragments/StudyFragment.kt
+++ app/src/main/java/com/example/snapstudy_android/fragments/StudyFragment.kt
@@ -1,10 +1,8 @@
 package com.example.snapstudy_android.fragments
 
-import android.animation.AnimatorInflater
-import android.animation.AnimatorSet
 import android.os.Bundle
 import android.view.*
-import androidx.core.view.GestureDetectorCompat
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.example.snapstudy_android.R
 import kotlinx.android.synthetic.main.fragment_study.*
@@ -12,26 +10,11 @@ import kotlinx.android.synthetic.main.fragment_study.*
 
 class StudyFragment : Fragment() {
 
-    lateinit var frontAnim: AnimatorSet
-    lateinit var backAnim: AnimatorSet
-    private lateinit var detector: GestureDetectorCompat
-    var isFront = true
-
+    private var isHid: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //setting scale
-        var scale:Float= resources.displayMetrics.density
-        cvAnswer.cameraDistance = 8000*scale
-        cvPrompt.cameraDistance=8000*scale
-
-        //front animation
-        frontAnim=AnimatorInflater.loadAnimator(activity,R.animator.front_animator) as AnimatorSet
-        backAnim=AnimatorInflater.loadAnimator(activity,R.animator.back_animator) as AnimatorSet
-
-        //setting the swipe detector
-        detector = GestureDetectorCompat(activity,CardGestureListener())
 
     }
 
@@ -42,52 +25,22 @@ class StudyFragment : Fragment() {
         // Inflate the layout for this fragment
 
         val studyView = inflater.inflate(R.layout.fragment_study,container,false)
+        var cardF:CardView = studyView.findViewById(R.id.cvAnswer)
 
 
-        return view
-    }
-
-//    override fun onTouchEvent(event:MotionEvent?):Boolean{
-//        if (detector.onTouchEvent())
-//    }
-
-    inner class CardGestureListener: GestureDetector.SimpleOnGestureListener(){
-        private val SWIPE_THRESHOLD = 100
-        private val SWIPE_VELOCITY_THRESHOLD = 100
-        override fun onFling(
-            downEvent: MotionEvent?,
-            moveEvent: MotionEvent?,
-            velocityX: Float,
-            velocityY: Float
-        ): Boolean {
-            var diffX=moveEvent?.x?.minus(downEvent!!.x) ?:0.0F
-            var diffY=moveEvent?.y?.minus(downEvent!!.y) ?:0.0F
-
-            if(Math.abs(diffX)>Math.abs(diffY)){
-                //this is a left or right swipe
-                if(Math.abs(diffX)>SWIPE_THRESHOLD && Math.abs(velocityX)>SWIPE_VELOCITY_THRESHOLD){
-                    if(diffX>0){
-                        //right swipe
-                        this@StudyFragment.onRightSwipe()
-                    }
-
-                    else{
-                        //left swipe
-                        this@StudyFragment.onLeftSwipe()
-                    }
-                }
+        cardF.setOnClickListener{
+            if(isHid){
+                tvFlashPrompt.alpha = 1.0f
+                isHid = false
             }
 
-            return super.onFling(downEvent, moveEvent, velocityX, velocityY)
+            else {
+                tvFlashPrompt.alpha = 0.0f
+                isHid = true
+            }
         }
-    }
 
-    private fun onLeftSwipe() {
-        TODO("Not yet implemented")
-    }
 
-    private fun onRightSwipe() {
-        TODO("Not yet implemented")
+        return studyView
     }
-
 }
